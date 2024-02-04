ee25ebacea536436682e29001ff5de0c5d04bcf9
Odame <kwesi.danquah@daaseng.com>
2024-02-04 22:40:55 +0000

    General code refactoring- application routes

---
+++ app/main/routes.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from app.services.forms import ContactForm
+from app.main import bp
+
+@bp.route('/')
+def index():
+    return render_template('index.html')
+
+@bp.route('/about')
+def about():
+    return render_template('about.html')
+
+@bp.route('/contact')
+def contact():
+    form = ContactForm()
+    return render_template('contact.html',form=form)
