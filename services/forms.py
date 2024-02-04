c45efccb9c86d5605b232250f16931ae834ef9f5
Odame <kwesi.danquah@daaseng.com>
2024-02-04 19:04:09 +0000

    Commented out unnecessary field

--- services/forms.py
+++ services/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField,PasswordField,BooleanField
-from wtforms.validators import DataRequired, Length, Email,EqualTo
+from wtforms import StringField,TextAreaField,SubmitField,EmailField,PasswordField,BooleanField
+from wtforms.validators import DataRequired, Email,EqualTo
 
 class ContactForm(FlaskForm):
-    email = StringField('Email',validators=[DataRequired(),Email()])
+    email = EmailField('Email',validators=[DataRequired(),Email()])
     subject = StringField('Subject',validators=[DataRequired()])
     message = TextAreaField('Message',validators=[DataRequired()])
     
@@ -15,9 +15,9 @@ class ContactForm(FlaskForm):
 class LoginForm(FlaskForm):
     # username = StringField('username',validators=[DataRequired(), Length(min=3, max=20)])
     
-    email = StringField('Email',validators=[DataRequired(),Email()])
+    email = EmailField('Email',validators=[DataRequired(),Email()])
     password = PasswordField('Password',validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
+    # confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
@@ -26,4 +26,4 @@ class ProjectForm(FlaskForm):
     title = StringField('Title',validators=[DataRequired()])
     # slug = StringField('Slug',validators=[DataRequired])
     description = StringField('Description',validators=[DataRequired()])
-    summary = TextAreaField('Summary',validators=[DataRequired()])
+    content = TextAreaField('Content',validators=[DataRequired()])
