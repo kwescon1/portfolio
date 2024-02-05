736ca370952c5c42f8e21c40211c0c80cc5a92d5
Odame <kwesi.danquah@daaseng.com>
2024-02-05 21:38:55 +0000

    add dummy route

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -3,20 +3,13 @@ from app.services.forms import LoginForm
 from app.admin import bp
 
 
-@bp.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
 
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('admin.index'))
-        else:
-            flash('Login unsuccessful. Please check username and password', 'danger')
-
-    return render_template('admin/login.html', form=form)
 
 
 @bp.route('/')
 def index():
     return "logged in"
+
+@bp.route('/add-procuct')
+def add_product():
+    return 'product add page'
